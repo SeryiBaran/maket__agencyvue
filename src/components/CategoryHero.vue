a5a0e1be61410362b4098dcaf4aca4bd2a38c57b
SeryiBaran <seryibaranminepy@yandex.ru>
2025-03-20 00:05:37 +0300

    color css dev tools

--- src/components/CategoryHero.vue
+++ src/components/CategoryHero.vue
@@ -4,17 +4,40 @@ const { title, subheader, bg = 0 } = defineProps<{
   subheader: string
   bg?: number
 }>()
+
+// const opacityp = ref(1)
+// const brightnessp = ref(1)
+// const ap = ref(0.2)
+const opacityp = ref(1)
+const brightnessp = ref(0.15)
+const ap = ref(0.035)
 </script>
 
 <template>
-  <div class="p-0 bg-[#ACFF24]/20 container">
-    <div class="categoryHero py-[120px] text-center w-full">
-      <h2 class="text-12 font-semibold">
-        {{ title }}
-      </h2>
-      <p class="text-4.5 tracking-[-0.6%] pt-3.5">
-        {{ subheader }}
-      </p>
+  <div class="p-0 container">
+    <div class="bg">
+      <div class="categoryHero py-[120px] text-center w-full">
+        <h2 class="text-12 font-semibold">
+          {{ title }}
+        </h2>
+        <p class="text-4.5 tracking-[-0.6%] pt-3.5">
+          {{ subheader }}
+        </p>
+        <div class="controls font-mono px-6 py-4 rounded-[10px] bg-green-900 flex flex-col gap-2 w-100 bottom-4 right-10 fixed">
+          <div class="flex justify-between">
+            <span>opacity 0-1 0.005:</span><span>{{ opacityp }}</span>
+          </div>
+          <input v-model="opacityp" type="range" min="0" max="1" step="0.005">
+          <div class="flex justify-between">
+            <span>brightness 0-1 0.005:</span><span>{{ brightnessp }}</span>
+          </div>
+          <input v-model="brightnessp" type="range" min="0" max="1" step="0.005">
+          <div class="flex justify-between">
+            <span>alphag 0-1 0.005:</span><span>{{ ap }}</span>
+          </div>
+          <input v-model="ap" type="range" min="0" max="1" step="0.005">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,26 +46,15 @@ const { title, subheader, bg = 0 } = defineProps<{
 .categoryHero {
   position: relative;
   z-index: 0;
+  background: rgba(172, 255, 36, v-bind(ap));
 }
-.categoryHero::before {
+.bg {
+  position: relative;
+}
+.bg::before {
   content: '';
   background: url('/assets/HeroBGRECTS.png');
-  opacity: 0.5;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  position: absolute;
-  z-index: -2;
-  pointer-events: none;
-}
-.categoryHero::after {
-  content: '';
-  background: url('/assets/AbstractDesign.png');
-  background-repeat: no-repeat;
-  background-position: bottom;
-  background-size: 100%;
-  opacity: 1;
+  opacity: 0.6;
   top: 0;
   left: 0;
   bottom: 0;
@@ -50,9 +62,23 @@ const { title, subheader, bg = 0 } = defineProps<{
   position: absolute;
   z-index: -1;
   pointer-events: none;
-
-  background-image: url('/assets/CategoryHeroLines00.png');
-  background-position: top left;
-  background-size: 300px;
+}
+.bg::after {
+  content: '';
+  background-image: url('/assets/CategoryHeroLines00.webp');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  /* opacity: 0.2; */
+  opacity: v-bind(opacityp);
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  position: absolute;
+  z-index: -2;
+  pointer-events: none;
+  /* filter: saturate(0) brightness(0.3); */
+  filter: saturate(0) brightness(v-bind(brightnessp));
 }
 </style>
