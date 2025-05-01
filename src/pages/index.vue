ae37046590f8f653b7c9ed18f833768daf16c1f1
SeryiBaran <seryibaranminepy@yandex.ru>
2025-05-02 02:12:30 +0300

    upd

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,51 @@
 <script setup lang="ts">
+import { useScroll } from '@vueuse/core'
+import { useMotion } from '@vueuse/motion'
 import Index01Hero from '~/pagesSections/index/Index01Hero.vue'
 import Index02WhoUseUs from '~/pagesSections/index/Index02WhoUseUs.vue'
 import Index03OurServices from '~/pagesSections/index/Index03OurServices.vue'
 import Index04WhyChooseUs from '~/pagesSections/index/Index04WhyChooseUs.vue'
 import Index05Reviews from '~/pagesSections/index/Index05Reviews.vue'
 import Index06FAQ from '~/pagesSections/index/Index06FAQ.vue'
+
 import Index07ContactUs from '~/pagesSections/index/Index07ContactUs.vue'
 
 defineOptions({
   name: 'IndexPage',
 })
+
+const targetRef = useTemplateRef('targetRef')
+
+const { y } = useScroll(window)
+
+const vw = computed(() => `${10 - y.value / 10}vw`)
+const vh = computed(() => `${100 - y.value / 3}vh`)
+const rotate = computed(() => y.value * 2)
+
+const motion = useMotion(targetRef, {
+  initial: {
+    y: '100vh',
+    x: '100vw',
+    rotate: 0,
+  },
+})
+
+function update([newVw, newVh, newRotate]: [string, string, number]) {
+  motion.apply({
+    y: newVh,
+    x: newVw,
+    rotate: newRotate,
+    transition: {
+      type: 'keyframes', // spring
+      // stiffness: 350,
+      // damping: 20,
+    },
+  })
+}
+
+update([vw.value, vh.value, rotate.value])
+
+watch([vw, vh, rotate], update)
 </script>
 
 <template>
@@ -17,6 +53,11 @@ defineOptions({
     <h1 class="text-xl left-0 top-0 absolute z-10000">
       АХТУНГ! Это пока даже не 10% макета!
     </h1>
+    <p
+      ref="targetRef" class="text-8xl text-red text-center w-100vw fixed z-10001"
+    >
+      ААА-ааа-ааа-ааа-ааа-ааа-ааа
+    </p>
     <Index01Hero />
     <Index02WhoUseUs />
     <Index03OurServices />
--- src/pagesSections/index/Index01Hero.vue
+++ src/pagesSections/index/Index01Hero.vue
@@ -51,6 +51,7 @@ refs.forEach((ref, index) => {
         <div v-motion-slide-top :delay="200" :duration="300" class="subheader text-4 text-greybrand-60 mt-7.5 px-7.5 py-4.5 border border-greybrand-15 rounded-2 border-solid bg-[#242424]/20 gap-1.5 items-center md:text-4.5 xl:text-5.5 max-md:leading-[150%] xl:mt-10 xl:px-10 xl:py-6 xl:rounded-[10px] md:flex">
           For <span class="subheaderSelected">Startups</span> , <span class="subheaderSelected">Enterprise leaders</span> , <span class="subheaderSelected">Media & Publishers</span> and <span class="subheaderSelected">Social Good</span>
         </div>
+        <!-- TODO: make cool buttons sitting animation -->
         <div class="font-medium pt-10 flex gap-[13px] items-center justify-center">
           <RouterLink ref="button1" to="/work" class="button buttonOutline">
             Our Works
